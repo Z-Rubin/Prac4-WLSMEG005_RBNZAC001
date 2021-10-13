f60f3935f9474a839db1bb6967fdcaffddfcc4ff
Z-Rubin <caznibur@gmail.com>
2021-10-13 13:27:58 +0100

    added button functionality

--- prac4.py
+++ prac4.py
@@ -6,14 +6,17 @@ import datetime
 import time
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
+import RPi.GPIO as GPIO
 
 global chanTemp 
 global chanLDR 
 global startingTime
+global samplingRate
 
 def setup():
     global chanTemp
     global chanLDR
+    global samplingRate
     # create the spi bus
     spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 
@@ -28,11 +31,31 @@ def setup():
     # create an analog input channel on pin 2
     chanLDR = AnalogIn(mcp, MCP.P2)
 
+    # create button event handler
+    samplingRate = 10
+    buttonPin = 25
+    GPIO.setup(buttonPin,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+
+    GPIO.add_event_detect(buttonPin, GPIO.FALLING,
+                        callback=changeSamplingRate,
+                        bouncetime=200)
+
+def changeSamplingRate(channel):
+    global samplingRate
+    samplingRates = [10, 5, 1]
+    print(samplingRate)
+    if samplingRate != 1:
+        samplingRate = samplingRates[samplingRates.index(samplingRate)+1]
+    else:
+        samplingRate = 10
+    print(samplingRate)
+
 def fetch_sensor_val():
     global chanTemp
     global chanLDR
     global startingTime
-    thread = threading.Timer(10.0, fetch_sensor_val)
+    global samplingRate
+    thread = threading.Timer(samplingRate, fetch_sensor_val)
     thread.daemon = True  # Daemon threads exit when the program does
     thread.start()
     currentTime = time.time()
